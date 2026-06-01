f3ef4456c4cd04b3a21dcc565679cd6cc7aa58dc
nainanijo <nainangelina@gmail.com>
2026-06-01 15:28:54 +0530

    Create script.js

---
+++ Internship/Day 1/JS-basics/script.js
@@ -0,0 +1,19 @@
+console.log("Hello Universe")
+document.write("howdy doo")
+//variable declaration
+/* Multi 
+    Line Comment */
+let name1="Naina"
+console.log(name1)
+
+let salary=30000000
+console.log(salary)
+console.log(typeof(salary))
+console.log(typeof(name1))
+
+let isValid // declaration
+isValid=true //assignment
+
+isValid=false
+console.log(isValid)
+
