79278c03c4f54bb61bbf0d6643502812f0a9f8e5
nainanijo <nainangelina@gmail.com>
2026-06-01 15:46:05 +0530

    Update script.js

--- Internship/Day 1/JS-basics/script.js
+++ Internship/Day 1/JS-basics/script.js
@@ -17,3 +17,32 @@ isValid=true //assignment
 isValid=false
 console.log(isValid)
 
+//let isValid=true
+//console.log(isValid)
+
+//const pi=3.14
+//console.log(pi)
+pi=3.1412
+
+console.log(200+100)
+console.log(200-100)
+console.log(200/100)
+console.log(pi**2)
+console.log(10%3)
+
+let a=150
+let b=45
+let c= a*b
+console.log(`Product of ${a} and ${b} is ${c}`) // `-> tilda symbol
+c=a++ //postfix increment
+console.log(c)
+console.log(a)
+
+d=++a //prefix increment
+console.log(d)
+console.log(a)
+
+let x=5
+x+=5 //x=x+5
+console.log(x)
+
